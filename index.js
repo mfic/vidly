09634ec0f8624eb582ffe25eafb211d822913dc8
Jan Quest <jan@quest-it.eu>
2021-05-17 22:08:58 +0200

    error logging and authenticaton

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+require('express-async-errors');
 const config = require('config');
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
 const express = require('express');
+const winston = require('winston/lib/winston/config');
 const app = express();
+require('./startup/logging')();
+require('./startup/database')();
+require('./startup/routes')(app);
 
-const uri = `mongodb+srv://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.uri}`;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(() => console.log('Connected to MonogoDB...'))
-    .catch((err) => console.error('Could not connect to MongoDB...', err.message));
+if (!config) {
+    console.error('FATAL ERROR: Config not loaded.')
+    process.exit(1);
+}
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-
-
-const port = process.env.PORT || config.PORT;
-app.listen(port, () => console.log(`Listen on port ${port}`));
+const port = config.PORT;
+app.listen(port, () => winston.info(`Listen on port ${port}`));
