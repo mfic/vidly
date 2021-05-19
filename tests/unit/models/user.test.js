27ac36177ae43d0bd116141640910ba53540ddbc
Jan Quest <jan@quest-it.eu>
2021-05-19 19:52:12 +0200

    add unit test for gerate token

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,19 @@
+const { User } = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const mongoose = require('mongoose');
+
+describe('user.generateToken', () => {
+  it('should return a valid jwt', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      name: 'a',
+      isAdmin: false
+    }
+    const user = new User(payload);
+    const token = user.generateToken()
+    const decoded = jwt.verify(token, config.jwtPrivateKey)
+
+    expect(decoded).toMatchObject(payload);
+  });
+});
